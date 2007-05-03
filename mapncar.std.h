c30806fe1579a7814510e52f768a28a7e631dee0
Jean-Marc.Molines <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2007-05-03 08:58:01 +0000

    JMM add parameter.std.h mapncar.std.h to record history after rhode compilation which mess up these files
    git-svn-id: https://servforge.legi.grenoble-inp.fr/svn/CHARTTOOLS/CHART/trunk@25 055b547e-6267-4d69-82d2-82d11ed5763d

---
+++ mapncar.std.h
@@ -0,0 +1,31 @@
+c --------------------------------------------------------------------
+c 
+c Programme      :  chart, coupe
+c Fichier        :  mapdata.h
+c 
+c Auteur         :  Eric Brown
+c
+c Description    :  Ce fichier contient le common ou se trouvent les
+c                   parametres NCAR initialises par mapint
+c 
+c --------------------------------------------------------------------
+
+
+      integer  sz_iama, sz_cra
+      parameter (sz_iama = 25000000)
+      parameter (sz_cra =  1000000)
+
+
+      integer  iama(sz_iama), iaia(10), igia(10)
+      real     xcra(sz_cra),ycra(sz_cra)
+
+      common /mapncar/iama,iaia,igia,xcra,ycra
+
+
+      integer SZ_IWRK, SZ_RWRK
+      parameter (SZ_IWRK=190000)
+      parameter (SZ_RWRK=190000)
+
+      integer iwrk (SZ_IWRK)
+      real    rwrk (SZ_RWRK)
+      common /contwork/ iwrk, rwrk
