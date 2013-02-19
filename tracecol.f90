29ba887dfb0f4d291a8159c086f7ac0ba532d5b2
Jean-Marc.Molines <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2013-02-19 11:12:23 +0000

    JMM : chamge LEGI-MEOM to LGGE-MEOM in the team
    git-svn-id: https://servforge.legi.grenoble-inp.fr/svn/CHARTTOOLS/trunk@179 055b547e-6267-4d69-82d2-82d11ed5763d

--- tracecol.f90
+++ tracecol.f90
@@ -495,7 +495,7 @@ CONTAINS
 
     ! -team option
     IF ( opt_team  ==  1 ) THEN
-       cldum = 'LEGI-MEOM'
+       cldum = 'LGGE-MEOM'
        CALL plchhq( 0.01, 0.02, cldum, -0.35, 0., -1.)
     ENDIF
 
